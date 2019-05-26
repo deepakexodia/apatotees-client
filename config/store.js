85baeec7441749e1c9f4fb29487a3631cdf2d8eb
deepaksingh <deepak17.singh@paytm.com>
2019-05-26 21:09:17 +0530

    hardcoded ajax base url

--- config/store.js
+++ config/store.js
@@ -1,7 +1,6 @@
-const serverUrl = process.env.SERVERURL || 'localhost:3001';
 // config used by store client side only
 module.exports = {
 	// store UI language
 	language: 'en',
-	ajaxBaseUrl: `https://${serverUrl}/ajax`
+	ajaxBaseUrl: `https://apatotees-server.herokuapp.com/ajax`
 };
